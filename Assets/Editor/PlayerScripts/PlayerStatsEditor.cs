fdf9e825273b5fe360e0f4f56b3579b30b31b5f5
Lordmatics <niallmaple@yahoo.co.uk>
2017-05-24 17:27:10 +0100

    Revamped Art + Added more enemies (Artwise)

---
+++ Assets/Editor/PlayerScripts/PlayerStatsEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerStats))]
+public class PlayerStatsEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.HelpBox("This script contains the players level info", MessageType.Info);
+
+        //base.OnInspectorGUI();
+
+        PlayerStats script = (PlayerStats)target;
+        if(script != null)
+        {
+            script.experience = EditorGUILayout.IntField("Experience", script.experience);
+            EditorGUILayout.LabelField("Level", script.level.ToString());
+        }
+    }
+}
--- Assets/Scripts/Utility/Databases/EnemyLibrary.cs
+++ Assets/Scripts/Utility/Databases/EnemyLibrary.cs
@@ -9,11 +9,16 @@ public static class EnemyLibrary
     public static string asteroid_02 = "Obstacles/Asteroid_02";
     public static string asteroid_03 = "Obstacles/Asteroid_03";
     public static string asteroid_04 = "Obstacles/Asteroid_04"; // Enemy Ship
+    public static string asteroid_05 = "Obstacles/Asteroid_05"; // Turbosquid Asteroid
+    public static string asteroid_06 = "Obstacles/Asteroid_06"; // Turbosquid Fighter
+    public static string asteroid_07 = "Obstacles/Asteroid_07"; // Turbosquid small suicide ship
+
+
 
 
     public static string GetRandomAsteroid()
     {
-        int rand = Random.Range(1, 5);
+        int rand = Random.Range(1, 8);
         return "Obstacles/Asteroid_0" + rand.ToString();
     }
 }
